314b4b42ab65e6fadfc1b0725df5fd1f4c4fee31
Devansh <devanshjindal31@gmail.com>
2025-01-30 20:59:23 +0530

    rectified

--- src/components/LeadershipCard/LeadershipCard.jsx
+++ src/components/LeadershipCard/LeadershipCard.jsx
@@ -11,7 +11,7 @@ export default function LeadershipCard({ leader }) {
                     className="w-28 h-36 lg:w-36 lg:h-44 xl:w-40 xl:h-48 mb-4 object-cover border-4 border-gray-200 rounded-lg shadow-sm"
                 />
                 <div className="flex flex-col items-start justify-start ml-6 max-h-full">
-                    <h3 className="text-lg md:text-xl xl:text-2xl font-bold text-gray-800">{leader.name}</h3>
+                    <h3 className="text-lg md:text-xl xl:text-2xl font-semibold text-gray-800">{leader.name}</h3>
                     <p className="text-xs md:text-sm xl:text-lg text-gray-500 text-start mb-4">{leader.position}</p>
                     <div className="flex justify-start space-x-4 ">
                         <a href={leader.profileLink} target="_blank" rel="noopener noreferrer">
